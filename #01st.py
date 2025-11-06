b780a5c6d927c932b01ca1f50ee65d6e77ff01ba
Usmanagani Davalasab Attar <usda9087@gmail.com>
2025-11-07 00:21:44 +0530

    first program

---
+++ #01st.py
@@ -0,0 +1,20 @@
+a=input('what is your name : ')
+b="hello" 
+c=" welcome to the USMAN's coding hub"
+print() #This will print Empty line
+print(f'{b} {a} {c}')
+print()
+print("Let's begin with the patern printing")
+print()
+print("Patern of your name is ")
+print()
+# # Function to print a right-angled triangle with increasing stars
+def print_right_triangle_increasing(rows):
+        for i in range(1, rows + 1):
+         print(f"{a} " * i)
+
+
+print_right_triangle_increasing(8)  # Prints a triangle with 1 to 8 names
+
+
+
