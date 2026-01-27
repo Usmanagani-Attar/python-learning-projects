4cfa58a300f4fb378bebbabf654b0bb0916d075c
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-28 00:03:53 +0530

    touples

---
+++ 6_coolections/27touples.py
@@ -0,0 +1,28 @@
+fruits = ("apple","mango","banana","banana","pineapple","orange","coconut")
+
+# print(dir(fruits))
+# print()
+
+# print(help(fruits))
+# print()
+
+print(len(fruits))
+print()
+
+print(fruits[3])
+print()
+
+print("apple" in fruits)
+print("grapes" in fruits)
+print()
+
+print()
+print(fruits)
+
+
+print()
+print(fruits.index("apple"))
+
+print(fruits.count("banana"))
+print(fruits)
+
