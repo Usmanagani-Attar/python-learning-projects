4c8f4db833a09ac47d5e83e3ceb1d898ccf3aec9
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-27 19:01:23 +0530

    looping

---
+++ 4_loops/20looping_over_string.py
@@ -0,0 +1,14 @@
+a="1234hello567dear"
+for i in a:
+    print(i)
+
+
+##########################################################
+    b= "hello dear coder how is your coding practise is going on!"
+
+    word=b.split()#splits word by word
+    for i in word:
+        print(i)
+
+
+        
