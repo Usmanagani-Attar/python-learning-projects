51991a300e3cabdce14e15dfc3ec7094432e27f7
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-28 00:04:19 +0530

    2dlists

---
+++ 6_coolections/28_2dlists.py
@@ -0,0 +1,21 @@
+fruits=["apple","coconut","pineapple","mango","orrange"]
+vegies = ["carrot","potatoes","tomatoes","brinjal"]
+meats = ["chicken","motton","fish","turkey","goat"]
+
+
+groceries = [fruits,vegies,meats]
+print()
+print(groceries)
+print()
+for i in groceries:
+    print(i)
+print()
+    
+print(groceries[0][3])
+print(groceries[0][3])
+print(groceries[1][2])
+print(groceries[2][1])
+print(groceries[1][3])
+
+
+
