c453f97804963f1e2a2f512de4beae7e022faceb
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-28 00:03:29 +0530

    sets

---
+++ 6_coolections/26sets.py
@@ -0,0 +1,18 @@
+fruits = {"apple","mango","banana","banana","pineapple","orange","coconut"}
+
+# print(dir(fruits))
+# print()
+
+# print(help(fruits))
+# print()
+
+print(len(fruits))
+print()
+
+
+print("apple" in fruits)
+print("grapes" in fruits)
+print()
+
+
+print(fruits.clear())
