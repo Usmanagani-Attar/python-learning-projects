dc9762c99943fe67a7ccdf2e4be8f4093de52db6
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-29 19:40:05 +0530

    random

---
+++ 7_random/30ran.py
@@ -0,0 +1,19 @@
+import random 
+
+number = random.randint(0,5)
+print(number)
+print()
+num=random.random()
+print(num)
+print()
+fruits = ["apple","mango","banana","banana","pineapple","orange","coconut"]
+ 
+
+choise=random.choice(fruits)
+
+print(choise)
+
+print()
+newfruits=random.shuffle(fruits)
+print(newfruits)
+
