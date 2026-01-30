24cb3d5abc2b5299bf02c78da09576ea7fada82b
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-01-31 00:06:21 +0530

    FUNCTIONS

---
+++ 8_functionssn/1_basic_in_functions/functions.py
@@ -0,0 +1,9 @@
+
+name = str(input("Enter your name : "))
+age = int(input("Enter your age : "))
+
+def hapybdy(name,age):
+    print(f"hello {name} you are now {age} years old")
+    return 10+age
+print(hapybdy(name,age))
+
