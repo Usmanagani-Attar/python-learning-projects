fc7d6a0cce9f354c8312704f4bcbeb1e0c968486
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-01 11:01:45 +0530

    dictionary

---
+++ 9_iterables/48dictionaries.py
@@ -0,0 +1,14 @@
+data={"A":1,"B":2,"C":3}
+for i in data:
+    print(i)
+print()
+
+for i,j in data.items():
+    print(f"{i} : {j}")
+
+print()
+
+print(data.items())
+print()
+
+print(data.values()) 
