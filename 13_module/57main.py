94ad2e8128944a0fb8aa6f2db1561d622a6c4a20
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-09 00:11:38 +0530

    main

---
+++ 13_module/57main.py
@@ -0,0 +1,19 @@
+import  example
+r=example.pi
+print(r)
+print()
+
+s = example.square(2)
+print(s)
+
+c=example.cube(2)
+print()
+print(c)
+
+d=example.circumference(25)
+print()
+print(d)
+
+e=example.area(20)
+print()
+print(e)
