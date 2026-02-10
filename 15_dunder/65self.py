61869ee5446383d1ab9ee33f48c3a7cb9073b30c
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-11 00:04:23 +0530

    self

---
+++ 15_dunder/65self.py
@@ -0,0 +1,15 @@
+class a:
+    def __init__(self,name,model,year,brand,colour):
+        self.name=name
+        self.model=model
+        self.year=year
+        self.brand=brand
+        self.colour=colour
+        
+b=a("ACE",2014,2023,"TATA","white")
+
+print(b.brand)
+print(b.name)
+print(b.model)
+print(b.colour)
+print(b.year)
