08f9e89c2830876039bf2433433240aa81b955fa
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-10 18:41:39 +0530

    enclosed

---
+++ 14_variablescope/60enclosed.py
@@ -0,0 +1,43 @@
+def func1():
+    e=1
+    print(e)
+    def func3():
+        print(e)
+    func3()
+
+
+def func2():
+    e=3
+    print(e)
+
+
+
+
+func1()
+print()
+func2()
+
+
+#############################OR################################
+print()
+print()
+
+
+def func9():
+    e=7
+    print(e)
+    def func3():
+        print(e)
+        
+        def func2():
+            print(e)
+        func2()
+    func3()
+
+
+
+func9()
+
+
+
+
