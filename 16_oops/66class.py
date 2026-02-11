c3c584a9cc666698b22fc14983353952767928bc
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-11 17:44:39 +0530

    class

---
+++ 16_oops/66class.py
@@ -0,0 +1,15 @@
+class student:
+    class_year =2025
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+student1=student("spongeberg",25)
+
+
+
+print(student1.name)
+print(student1.age)
+
+print()
+print(student.class_year)
