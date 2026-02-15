82d8a46dca5072c64c95932cd3adacf7e2d6e31d
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-15 11:42:37 +0530

    multi inheritance

---
+++ 16_oops/69multiineritance2.py
@@ -0,0 +1,45 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+
+    def eat(self):
+        print(f"    1{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+class Hawk(Predator):
+    pass
+
+class Fish(Prey,Predator):
+    pass
+
+
+rabbit = Rabbit("Bugs")
+hawk=Hawk("Tony")
+fish=Fish("Nemo")
+
+rabbit.flee()
+rabbit.eat()
+rabbit.sleep()
+print()
+
+hawk.hunt()
+hawk.sleep()
+hawk.eat()
+print()
+
+fish.hunt()
+fish.eat()
+fish.sleep()
+fish.flee()
