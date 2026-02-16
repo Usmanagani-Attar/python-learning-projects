d92f01efa755befe6068619b41fd7ae97ae48f3d
Usmanagani Davalasab Attar <usda9087@gmail.com>
2026-02-16 15:13:23 +0530

    prpertty

---
+++ 16_oops/75property.py
@@ -0,0 +1,49 @@
+class Rectangle :
+    def __init__(self,width,height):
+        self._width=width 
+        self._height=height
+
+    @property
+    def width(self):
+        return f"{self._width:.1f} cm"
+    
+    
+    @property
+    def height(self):
+        return f"{self._height:.1f} cm"
+        
+
+    @width.setter
+    def width(self,new_width):
+        if new_width<0:
+            print("Width can not be less than zero")
+        else:
+            self._width=new_width
+
+
+    @height.setter
+    def height(self,new_height):
+        if new_height<0:
+            print("Height can not be less than zero")
+        else:
+            self._height=new_height
+
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width has been deleted")
+
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height has been deleted")
+
+
+rectangle=Rectangle(3,4)
+
+rectangle.height=-1
+del rectangle.width
+
+#print(rectangle.width)
+print(rectangle.height)
